ad7756855b2fa3d27527c7f67369f541f0fe0698
Nemmatee <nemmatee2004@gmail.com>
2025-12-01 22:21:20 +0100

    added one comment in app.java

--- src/main/java/hu/bme/orlog/App.java
+++ src/main/java/hu/bme/orlog/App.java
@@ -6,6 +6,8 @@ import hu.bme.orlog.ui.OrlogFrame;
 
 public class App {
     public static void main(String[] args){
+        // Start the Swing user interface on the Event Dispatch Thread,
+        // which is the recommended way to create and show Swing windows.
         SwingUtilities.invokeLater(() -> new OrlogFrame().setVisible(true));
     }
 }
